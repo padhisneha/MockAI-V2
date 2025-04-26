ddcef6717b5784065a84940b4df9f372e5e81499
Sneha Padhi <130724380+padhisneha@users.noreply.github.com>
2025-04-26 16:47:42 +0530

    show feedback

--- app/(root)/interview/[id]/page.tsx
+++ app/(root)/interview/[id]/page.tsx
@@ -29,8 +29,8 @@ const page = async ({ params }: RouteParams) => {
             </div>
 
             <Agent 
-                userName={user?.name}
-                type={user?.id}
+                userName={user?.name || ''}
+                userId={user?.id}
                 interviewId={id}
                 type="interview"
                 questions={interview.questions}
